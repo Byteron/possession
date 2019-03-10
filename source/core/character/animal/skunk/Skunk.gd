f23550ce74ef0889924ee849ced19d845841932f
Aaron Winter <winter.aaron93@gmail.com>
2019-03-11 00:43:57 +0100

    added gas and second city layer in background

--- source/core/character/animal/skunk/Skunk.gd
+++ source/core/character/animal/skunk/Skunk.gd
@@ -1,10 +1,23 @@
 extends PlayerCharacter
 
+onready var gas = $Gas
+
+func interact():
+	gas.emit()
+
+func flip_left():
+	.flip_left()
+	gas.scale.x = 1
+	gas.position.x = 4
+
+func flip_right():
+	.flip_right()
+	gas.scale.x = -1
+	gas.position.x = -12
+
 func _setup_states():
 	._setup_states()
 	states.idle = $States/Idle
 	states.walk = $States/Walk
 	states.jump = $States/Jump
 	states.fall = $States/Fall
-
-	
