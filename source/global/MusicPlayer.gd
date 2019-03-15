5ea9cbd0d12cfb1ddd987571eac63ff605609680
Aaron Winter <winter.aaron93@gmail.com>
2019-03-15 12:25:27 +0100

    added music player

---
+++ source/global/MusicPlayer.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var playlist = {
+	menu_theme = preload("res://audio/music/Menu_Loop.wav"),
+	bar_theme = preload("res://audio/music/bar_loop.wav")
+}
+
+var next_track = null
+
+onready var tween = $Tween
+onready var audio = $AudioStreamPlayer
+
+func play(track):
+	if audio.stream == track:
+		return
+	
+	next_track = track
+	tween.interpolate_property(audio, "volume_db", audio.volume_db, -72, 0.25, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	tween.start()
+
+func _on_Tween_tween_completed(object, key):
+	if next_track:
+		audio.stream = next_track
+		next_track = null
+		tween.interpolate_property(audio, "volume_db", -72, AudioServer.get_bus_volume_db(1), 0.25, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+		tween.start()
+		audio.play()
