dcd284eafb5e7873d9166f96e16bd12ffdf58c0f
CorvatoCafeto <emma.fs92@gmail.com>
2019-03-17 00:13:03 -0300

    Levels!!!!

--- source/global/Scene.gd
+++ source/global/Scene.gd
@@ -6,8 +6,13 @@ var Game = "res://source/core/Game.tscn"
 var Intro = "res://source/interface/story/stories/Intro.tscn"
 var TitleScreen = "res://source/interface/menus/TitleScreen.tscn"
 var Options = "res://source/interface/menus/Options.tscn"
-var Ending = "res://source/interface/story/Ending.tscn"
+var inter01 = "res://source/interface/story/Intermission01.tscn"
+var inter02 = "res://source/interface/story/Intermission02.tscn"
+var inter03 = "res://source/interface/story/Intermission03.tscn"
+var level04 = "res://source/core/levels/04_First.tscn"
 var level05 = "res://source/core/levels/05_Second.tscn"
+var Ending = "res://source/interface/story/Ending.tscn"
+
 
 # GLOBAL
 
